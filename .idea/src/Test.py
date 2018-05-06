b7b6500a98915d706eeb721426d91b5d1a2a591f
sivan.as <sivan.as@mobfox.com>
2018-05-06 17:21:46 +0200

    Add config file

--- .idea/src/Test.py
+++ .idea/src/Test.py
@@ -1,129 +1,107 @@
+import ConfigParser
+import os
 import pandas as pd
 import seaborn as sns
-from enum import Enum
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from scipy import stats
 import logging
 
-class OutputType(Enum):
-    PDF = '/Users/sivan.as/Downloads/prodtestats.pdf'
-    HTML = '/Users/sivan.as/Downloads/prodtestats.html'
-
-
-OUTPUT_TYPE = OutputType.PDF
-# OUTPUT_TYPE = OutputType.HTML
-
+# Constants
+CONTROL = "Control"
+TEST = "Test"
 P_VALUE_PRECISION = 3
-NORMAL_P_VALUE = 0.05
+ALPHA_VALUE = 0.05
 
-REPORT_TITLE = "Report for prodtest-ssp-12345-example"
-CSV_INPUT_FILE_PATH = '/Users/sivan.as/Downloads/3ccf5ebf-d2c2-4f7b-ba0c-58b7feb2893d.csv'
+# prepare configuration from config file
+def prepare_conf() :
+    config_parser = ConfigParser.RawConfigParser()
+    conf_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Config.txt')
+    config_parser.read(conf_file)
+    return config_parser
 
-TEST_SERVERS = [
-    'i-0ce10dc8209c58a28',
-    'i-0ddf0976483394643',
-    'i-0828061826d38ad8d',
-    'i-04ec2dc0f13cc8be8',
-    'i-09f4bb20b2ccf8566',
-    'i-042388cecaa737d56',
-    'i-08be6e52c201af9f6',
-    'i-0dd7d0ddcae6be9a4',
-    'i-0b8357dea675a2cd1',
-    'i-058d261d921da4b02'
-]
 
-COLUMN_NAMES = [
-    'total_requests',
-    'total_ex_bid_requests',
-    'total_ex_valid_bid_requests',
-    'total_ex_timeouts',
-    'total_ex_nobids',
-    'total_ex_errors',
-    'total_ex_bids',
-    'total_ex_valid_bids',
-    'total_ex_discardedbids',
-    'total_served',
-    'fill_rate',
-    'total_impressions',
-    'total_ex_bid_impressions',
-    'total_ex_bid_impression_errors',
-    'ex_total_impression_price',
-    'render_rate',
-    'total_revenue',
-    'ecpm',
-    'total_clicks',
-    'click_rate',
-    'avg_request_duration',
-    'avg_served_duration',
-    'avg_click_duration'
-]
+# prepare logger
+def prepare_logger() :
+    logger = logging.getLogger('prodtest')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(logging.StreamHandler())
+    return logger
+
+
+config_parser = prepare_conf()
+logger = prepare_logger()
 
 
 # Main
 def main():
-    print("Setting up plot configuration")
+    input = config_parser.get('data', 'input_file_path')
+    logger.info("Reading input file from path: {}".format(input))
+    csv_file = pd.read_csv(input, skip_blank_lines=True)
+
+    logger.info("Parsing control and test group data by adserver_host")
+    test_servers = config_parser.get('data', 'test_servers').split(",")
+    control_group = csv_file.loc[csv_file['adserver_host'].isin(test_servers) == False]
+    test_group = csv_file.loc[csv_file['adserver_host'].isin(test_servers)]
+
+    logger.info("Setting up plot configuration")
     plot_config()
-    print("Reading input file from path: {}".format(CSV_INPUT_FILE_PATH))
-    csv_file = pd.read_csv(CSV_INPUT_FILE_PATH, skip_blank_lines=True)
-    print("Parsing control and test group data by adserver_host")
-    control_group = csv_file.loc[csv_file['adserver_host'].isin(TEST_SERVERS) == False]
-    test_group = csv_file.loc[csv_file['adserver_host'].isin(TEST_SERVERS)]
-    print("Generating {} report".format(OUTPUT_TYPE))
-    generate_report(control_group, test_group, OUTPUT_TYPE)
+
+    output_type = config_parser.get('data', 'output_type')
+    logger.info("Generating {} report".format(output_type))
+    generate_report(csv_file, control_group, test_group, output_type)
 
 
 # Create design configuration for seaborn plot
 def plot_config():
-    sns.set(color_codes=True)
-    sns.set_style('whitegrid')
-    sns.set_palette('Set2')
+    sns.set_style(config_parser.get('design', 'seaborn_style'))
+    sns.set_palette(config_parser.get('design', 'seaborn_palette'))
 
 
 # Check if the data is distributed normally
 def is_normal_distribution(data):
     norm_res, norm_pval = stats.shapiro(data)
-    is_normal_distribution = norm_pval > NORMAL_P_VALUE;
+    is_normal_distribution = norm_pval > ALPHA_VALUE;
     return is_normal_distribution
 
 
 # Run a stats test according to the distribution of the data
 def do_test(control_data, test_data, is_normal_distribution):
-    report_text = "Distribution: "
+    report_text = "Distribution Type: {}\nTest Performed: {}\nTest Summary: {}\n\nTest Details\nControl Group: {}\n\nTest Group: {}\n"
     if (is_normal_distribution):
-        report_text += "The data is normally distributed \nRunning Test: T-Test\n"
+        dist_type = "Normal Distribution"
+        test_type = "T-Test"
         test_res, test_pval = stats.ttest_ind(control_data, test_data, axis=0, equal_var=False)
     else:
-        report_text += "The data is NOT normally distributed \nRunning Test: Mann-Whitney U Test\n"
+        dist_type = "Unknown Distribution"
+        test_type = "Mann-Whitney U Test"
         test_res, test_pval = stats.mannwhitneyu(control_data, test_data)
 
-    report_text += "Test Status: "
-
-    is_significant_diff = test_pval < NORMAL_P_VALUE
+    is_significant_diff = test_pval < ALPHA_VALUE
     if (is_significant_diff):
-        report_text += "The difference is significant with p-value {}".format(test_pval)
+        test_summary = "The difference is significant with p-value {}".format(test_pval)
     else:
-        report_text += "All Good!"
+        test_summary = "All Good!"
 
-    report_text += "\nDetails: \nControl:\n{}\nTest:\n{}".format(control_data.describe(), test_data.describe())
-
+    report_text = report_text.format(dist_type, test_type, test_summary, control_data.describe(), test_data.describe())
     return report_text
 
 
 # Create a distribution plot from data
 def create_distribution_plot(pdf, column_name, control_data, test_data):
-    sns.distplot(control_data, label="Control")
-    sns.distplot(test_data, label="Test")
+    sns.distplot(control_data, label=CONTROL)
+    sns.distplot(test_data, label=TEST)
     plt.legend()  # Show lables
-    plt.title("Distribution for " + column_name)
+    plt.title("Distribution for ".format(column_name))
     save_curr_figure_and_close(pdf)
 
 
-def create_time_plot(pdf, column_name, group_name, data):
+# Create a plot with the behaviour or each instance
+def create_instance_behaviour_plot(pdf, column_name, group_name, data):
     #time = pd.to_datetime(data['tslice'])
     adserver_data = pd.DataFrame(data=data).groupby(['tslice', 'adserver_host'])
-    plot = adserver_data.agg(column_name).sum().unstack().plot()
-    plt.title("{} Per AdServer ({})".format(column_name, group_name))
+    adserver_data.agg(column_name).sum().unstack().plot()
+    plt.title("{} Per Test Instance ({})".format(column_name, group_name))
     plt.xlabel("Time")
     plt.ylabel(column_name)
     save_curr_figure_and_close(pdf)
@@ -131,8 +109,8 @@ def create_time_plot(pdf, column_name, group_name, data):
 # Create a box plot from data
 def create_boxplot(pdf, column_name, control_data, test_data):
     boxplot_data = pd.DataFrame({
-        "Control": control_data,
-        "Test": test_data
+        CONTROL : control_data,
+        TEST : test_data
     })
     sns.boxplot(data=boxplot_data, orient="v")
     plt.xlabel("Experiment Groups")
@@ -148,53 +126,61 @@ def save_curr_figure_and_close(pdf):
 
 # Add report text to current pdf figure
 def add_report_text(pdf, column_name, text):
-    plt.gcf().text(0.05, 0.83, "Evaluating {} \n".format(column_name), transform=plt.gcf().transFigure, size=18)
+    plt.gcf().text(0.05, 0.85, "Evaluating {} \n".format(column_name), transform=plt.gcf().transFigure, size=18)
     plt.gcf().text(0.05, 0.1, text, transform=plt.gcf().transFigure)
     save_curr_figure_and_close(pdf)
 
 
-# Generate the A/B test report in PDF format
-def generate_pdf_report(control_group, test_group):
-    with PdfPages(OutputType.PDF.value) as pdf:
-        # Add Document Title
-        plt.gcf().text(0.5, 0.5, REPORT_TITLE, transform=plt.gcf().transFigure,
-                       size=24, ha="center")
-        save_curr_figure_and_close(pdf)
+# Add report summary to first page
+def add_report_summary(output_file) :
+    report_file = PdfFileReader(output_file, 'rb')
+    report_file.addPage()
 
+
+# Generate the A/B test report in PDF format
+def generate_pdf_report(csv_file, control_group, test_group):
+    prodtest_name = config_parser.get('data', 'prodtest_name')
+    output_file = config_parser.get('data', 'output_dir') + prodtest_name + ".pdf"
+    with PdfPages(output_file) as pdf:
+        first_page=plt.gcf()
+        first_page.text(0.5, 0.5, prodtest_name, transform=first_page.transFigure,
+                        size=24, ha="center")
+        save_curr_figure_and_close(pdf)
         # Loop through the columns and test each
-        for column_name in COLUMN_NAMES:
-            print("Creating AdServer activity plot for {}".format(column_name))
-            create_time_plot(pdf, column_name, "Control", control_group)
-            create_time_plot(pdf, column_name, "Test", test_group)
+        columns = list(csv_file.columns.values)
+        start_from_column = 2 #skip tslice and adserver_host
+        for i in range(start_from_column, len(columns)) :
+            column_name = columns[i]
+            logger.info("Creating AdServer activity plot for {}".format(column_name))
+
+            create_instance_behaviour_plot(pdf, column_name, CONTROL, control_group)
+            create_instance_behaviour_plot(pdf, column_name, TEST, test_group)
 
             control_data = control_group[column_name]
             test_data = test_group[column_name]
 
-            # print "Control: \n {} \n Test: \n {} \n ".format(control_data, test_data)
-
-            print("Checking distribution type for {}".format(column_name))
+            logger.info("Checking distribution type for {}".format(column_name))
             is_norm = is_normal_distribution(control_data) and is_normal_distribution(test_data)
             report_text = do_test(control_data, test_data, is_norm)
 
-            print("Creating distribution plot for {}".format(column_name))
+            logger.info("Creating distribution plot for {}".format(column_name))
             create_distribution_plot(pdf, column_name, control_data, test_data)
             add_report_text(pdf, column_name, report_text)
-            print("Creating box plot for {}".format(column_name))
+            logger.info("Creating box plot for {}".format(column_name))
             create_boxplot(pdf, column_name, control_data, test_data)
-            print('\n ================== \n')
+            logger.info('\n ================== \n')
 
-    print("Finished.")
+    logger.info("Finished.")
 
 # Generate the A/B test report in HTML format
 def generate_html_report(control_group, test_group):
-    print "html report"
-
+    logger.info("html report")
 
 # Generate a report containing the plots and additional info about the A/B test
-def generate_report(control_group, test_group, output_type):
-    if (output_type == OutputType.PDF):
-        generate_pdf_report(control_group, test_group)
-    elif (output_type == OutputType.HTML):
+def generate_report(csv_file, control_group, test_group, output_type):
+    if (output_type == 'pdf'):
+        generate_pdf_report(csv_file, control_group, test_group)
+    elif (output_type == 'html'):
         generate_html_report(control_group, test_group)
     else:
         raise Exception('Unsupported output file type for test report: ' + output_type)
